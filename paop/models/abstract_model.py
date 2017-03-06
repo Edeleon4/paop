7423635f9badeb62c3a411937f9b12efc0827329
Eduardo De Leon <edeleon@rosetta2.csail.mit.edu>
2017-03-06 10:06:32 -0500

    Base interface for Models

---
+++ paop/models/abstract_model.py
@@ -0,0 +1,42 @@
+print('abs model loaded')
+from models.data import Data
+"""
+    Model interface base
+        Exposed API:
+            train: Trains the model on inputted Training set
+            evaluate: Evaluates the model on the Dev set
+            summary: Returns summary data about the model
+            save(path): Store the model at the given path
+
+            stati:
+                load
+"""
+class AbstractModel():
+    name = "AbstractModel"
+    def load():
+        raise NotImplementedError()
+
+    def __init__(self, train_path, dev_path, test_path,
+                 debug=False):
+        self.debug = debug
+        self.data, self.props = self._get_data(train_path, dev_path, test_path)
+        self.model = get_model()
+
+    def train(self):
+        raise NotImplementedError()
+
+    def evaluate(self):
+        raise NotImplementedError()
+
+    def summary(self):
+        raise NotImplementedError()
+
+    def save(self):
+        raise NotImplementedError()
+
+    def _get_data(self, train_path, dev_path, test_path):
+        raise NotImplementedError()
+
+    def get_model(self):
+        raise NotImplementedError()
+
---
+++ paop/models/svm_models.py
@@ -0,0 +1,53 @@
+import numpy as np
+from random import shuffle
+import json
+from sklearn import svm
+from models.data import Data
+from models.abstract_model import AbstractModel
+from loaders.findings_svm_loaders import BOWs_Loader, AVG_Embeds_Loader
+
+class AbstractSVM(AbstractModel):
+    name = "SVM"
+    #__init__ properties data
+    def setup(self):
+        self.model = svm.SVC()
+
+    def train(self):
+        print(len(self.data.train_x))
+        self.model.fit(
+            self.data.train_x,
+            self.data.train_y
+        )
+
+    def evaluate(self):
+        score = self.model.score(self.data.dev_x, self.data.dev_y)
+        return score
+
+class SVM(AbstractSVM):
+    def __init__(self, train_path, dev_path, test_path, debug=False) :
+        super().__init__(train_path, dev_path, test_path, debug)
+
+    def get_data(self, train_path, dev_path, test_path):
+        train_x = None
+        train_y = None
+        dev_x = None
+        dev_y = None
+        test_x = None
+        test_y = None
+        with open(train_path, 'r') as file:
+            data = json.loads(file.read())
+            print(len(data['x']))
+            train_x = np.array(data['x'])
+            train_y = np.array(data['y'])
+
+        with open(dev_path, 'r') as file:
+            data = json.loads(file.read())
+            dev_x = np.array(data['x'])
+            dev_y = np.array(data['y'])
+
+        with open(test_path, 'r') as file:
+            data = json.loads(file.read())
+            test_x = np.array(data['x'])
+            test_y = np.array(data['y'])
+        return Data(train_x, train_y, dev_x, dev_y, test_x, test_y), {}
+
